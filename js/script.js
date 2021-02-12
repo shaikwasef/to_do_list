734795129c1e0ef0f0595f4fd2755e8dc3f72557
wasefshaik@gmail.com <wasefshaik@gmail.com>
2021-02-12 17:20:12 +0530

    Incomplete task function added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 var categories = document.getElementById("categories");
 var add = document.getElementById("add_button");
 var list = document.querySelector(".list");
+var task_field = document.getElementById("task_field");
 
 add.addEventListener('click',add_to_list);
 
 
 
 function add_to_list(){
-	// list_element = document.createElement("div");
-	// list_element.classList.add("list_element");
-	// list.append(list_element)
-	var value = categories.options[categories.selectedIndex].textContent ;
-	window.alert(value);
+	if (task_field.value == ""){
+		window.alert("Please define task!");
+		return ;
+	}
+	list_element = document.createElement("div");
+	list_element.classList.add("list_element_incomplete");
+	if(categories.options[categories.selectedIndex].textContent == "Incomplete"){
 
+		list_element.classList.add("list_element_incomplete");
+		list_element.textContent = task_field.value + " - Incomplete";
+		document.getElementById("task_field").value= "";
+	}
+	list.append(list_element);	
 }
