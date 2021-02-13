dd2ef38c496f64dca57baf2157c6e363e94921bc
wasefshaik@gmail.com <wasefshaik@gmail.com>
2021-02-13 16:00:17 +0530

    First draft to-do list ready

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@ var categories = document.getElementById("categories");
 var add = document.getElementById("add_button");
 var list = document.querySelector(".list");
 var task_field = document.getElementById("task_field");
+var all_button = document.getElementById("all_button");
+var upcoming_button = document.getElementById("upcoming_button");
+var completed_button = document.getElementById("complete_button");
+var pending_button = document.getElementById("pending_button");
+var root = document.querySelector(":root");
 
 add.addEventListener('click',add_to_list);
 
+var list_element_new = null ; //creating null element to conduct the order of task ; updated after every addition to last list_element
 
 
 function add_to_list(){
@@ -12,13 +18,58 @@ function add_to_list(){
 		window.alert("Please define task!");
 		return ;
 	}
-	list_element = document.createElement("div");
-	list_element.classList.add("list_element_incomplete");
-	if(categories.options[categories.selectedIndex].textContent == "Incomplete"){
-
-		list_element.classList.add("list_element_incomplete");
-		list_element.textContent = task_field.value + " - Incomplete";
-		document.getElementById("task_field").value= "";
+	if (categories.options[categories.selectedIndex].textContent  == "STATUS"){
+		window.alert("Please define category!");
+		return ;
 	}
-	list.append(list_element);	
+	list_element = document.createElement("div");
+	list_element.classList.add("list_element");
+	if(categories.options[categories.selectedIndex].textContent == "Upcoming"){
+		list_element.classList.add("list_element_upcoming");
+		list_element.textContent = task_field.value + " : upcoming";
+		document.getElementById("task_field").value= "";
+		list.insertBefore(list_element,list_element_new);
+		list_element_new = list_element ;
+	}
+	if(categories.options[categories.selectedIndex].textContent == "Completed"){
+		list_element.classList.add("list_element_complete");
+		list_element.textContent = task_field.value + " : completed";
+		document.getElementById("task_field").value= "";
+		list.insertBefore(list_element,list_element_new);
+		list_element_new = list_element ;
+	}	
+	if(categories.options[categories.selectedIndex].textContent == "Pending"){
+		list_element.classList.add("list_element_pending");
+		list_element.textContent = task_field.value + " : pending";
+		document.getElementById("task_field").value= "";
+		list.insertBefore(list_element,list_element_new);
+		list_element_new = list_element ;
+	}
+}
+
+upcoming_button.addEventListener('click',upcoming_list);
+complete_button.addEventListener('click',complete_list)
+pending_button.addEventListener('click',pending_list);
+all_button.addEventListener('click',all_list);
+
+
+function upcoming_list(){
+	root.style.setProperty('--upcoming_state','block');
+	root.style.setProperty('--pending_state','none');
+	root.style.setProperty('--completed_state','none');
+}
+function complete_list(){
+	root.style.setProperty('--upcoming_state','none');
+	root.style.setProperty('--pending_state','block');
+	root.style.setProperty('--completed_state','none');
+}
+function pending_list(){
+	root.style.setProperty('--upcoming_state','none');
+	root.style.setProperty('--pending_state','none');
+	root.style.setProperty('--completed_state','block');
+}
+function all_list(){
+	root.style.setProperty('--upcoming_state','block');
+	root.style.setProperty('--pending_state','block');
+	root.style.setProperty('--completed_state','block');
 }
